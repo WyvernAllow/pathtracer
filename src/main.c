caf3f75fa9007284de9f2d004fa40d4948947d7d
WyvernAllow <bloodflame2535@gmail.com>
2024-09-27 11:17:56 +0200

    remove read_file from main

--- src/main.c
+++ src/main.c
@@ -14,28 +14,6 @@ static const char *EXTENSIONS[] = {
     VK_EXT_DEBUG_UTILS_EXTENSION_NAME
 };
 
-static const uint8_t *read_file(const char *filename, size_t *len) {
-    FILE *file = fopen(filename, "rb");
-    if(!file) {
-        perror(filename);
-        return NULL;
-    }
-
-    fseek(file, 0, SEEK_END);
-    size_t file_size = (size_t)ftell(file);
-    fseek(file, SEEK_SET, 0);
-
-    uint8_t *buffer = malloc(sizeof(uint8_t) * file_size);
-    size_t bytes_read = fread(buffer, sizeof(uint8_t), file_size, file);
-    if(bytes_read != file_size) {
-        perror(filename);
-        return NULL;
-    }
-
-    *len = file_size;
-    return buffer;
-}
-
 static VKAPI_ATTR VkBool32 debug_callback(
     VkDebugUtilsMessageSeverityFlagBitsEXT           messageSeverity,
     VkDebugUtilsMessageTypeFlagsEXT                  messageTypes,
@@ -197,7 +175,7 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    
+
 
     vkDestroyDebugUtilsMessengerEXT(instance, messenger, NULL);
     vkDestroyInstance(instance, NULL);
